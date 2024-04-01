bc4890ac1ab336115938c34219dbdbfae07b34b4
hoanghuynh124 <165623638+hoanghuynh124@users.noreply.github.com>
2024-04-01 10:39:48 +0700

    Finish
    Huynh

--- js/code.js
+++
@@ -1,9 +0,0 @@
-class Person {
-    constructor(firstname, lastname) {
-    this.firstname = firstname;
-    this.lastname = lastname;
-    }
-    greet() {
-    console.log("Hi there! I'm", this.firstname);
-    }
-   }
